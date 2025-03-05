6081349920e2bfac2e03d1f880d376e691299fb3
git <samuelmataarias@elpuig.xeill.net>
2025-03-05 17:00:03 +0100

    se crearon 4 metodos

---
+++ src/newgit.java
@@ -0,0 +1,20 @@
+public class newgit {
+    public static void main(String[] args) {
+
+        method1();
+        method2();
+        method3();
+        method4();
+    }
+    public static void method1(){
+
+    }
+    public static void method2() {
+    }
+    public static void method3() {
+    }
+    public static void method4() {
+    }
+}
+
+
